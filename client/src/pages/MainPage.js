73ca4238e4c362f1a9e07d3d1ae14a08313d7334
Suneth <94039078+SunethMadhuwantha@users.noreply.github.com>
2025-02-04 16:43:31 +0530

    frontend view

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React from "react";
 
 export default function MainPage() {
   return (
     <div>
-      <h1>main page</h1>
+      <h1 className="lg:mx-32 text-5xl font-bold text-green-500">
+        Convert Your Currencies Today
+      </h1>
+      <p className="lg:mx-32 opacity-40 py-6">
+        Welcome to "Convert Your Currencies Today" !! This application allows
+        you to easily convertc urrencies based on the latest exchange
+        rates.Whether you're looking to buy or sell, or simply need to check the
+        current exchange rates, this app is perfect for you.
+      </p>
+
+      <div className="mt-5 flex items-center justify-center flex-col">
+        <section className="w-full lg:w-1/2 ">
+          <form>
+            <div className="mb-4">
+              <label
+                htmlFor=""
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Date
+              </label>
+              <input
+                type="Date"
+                id=""
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder=""
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor=""
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Source Currency
+              </label>
+              <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder=""
+                required><option value="">Select currency</option>
+
+                </select>
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor=""
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Target Currency
+              </label>
+              <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder="elect Target currency"
+                required><option value="">Select Target currency</option>
+
+                </select>
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor=""
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Amount in source currency
+              </label>
+              <input
+                type="text"
+                id=""
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder="Amount in source currency"
+                required
+              />
+            </div>
+            <button className="bg-green-500 hover:bg-green-700 font-medium text-white py-2 px-4 rounded-md">Get the target currency</button>
+            
+          </form>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
